6c206ec54900d1825815ea3aae0439d26d13f7c9
Ulises Orozco <ulisesorozco6@gmail.com>
2023-04-17 21:00:32 -0700

    Created Device and Device grid components with no data in them.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 import NavigationBar from '@components/NagivationBar';
 import { Welcome } from '@components/Welcome/Welcome';
+import {
+    QueryClient,
+    QueryClientProvider,
+} from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 export default function HomePage() {
     return (
         <>
-            <NavigationBar />
-            <Welcome />
+            <QueryClientProvider client={queryClient}>
+                <NavigationBar />
+                <Welcome />
+            </QueryClientProvider>
+
         </>
     );
 }
