f28209ff094e0dbeb3ed88960829d6e866042e10
Ulises Orozco <ulisesorozco6@gmail.com>
2023-04-20 16:12:49 -0700

    Created filter dropdown styles file to adjust margins.

---
+++ components/FilterDropdown/FilterDropdown.styles.ts
@@ -0,0 +1,14 @@
+import { createStyles } from '@mantine/core';
+
+const useStyles = createStyles(() => ({
+  productLabel: {
+    color: '#000000',
+    fontWeight: 700,
+  },
+
+  checkboxItems: {
+    marginTop: '.5rem',
+  },
+}));
+
+export default useStyles;
--- components/FilterDropdown/FilterDropdown.tsx
+++ components/FilterDropdown/FilterDropdown.tsx
@@ -1,9 +1,10 @@
-import { Button, Menu } from '@mantine/core';
-import { IconSettings } from '@tabler/icons-react';
+import { Button, Checkbox, Menu } from '@mantine/core';
 import { useState } from 'react';
+import useStyles from './FilterDropdown.styles';
 
 export const FilterDropdown = () => {
   const [opened, setIsOpened] = useState(false);
+  const { classes } = useStyles();
 
   return (
     <Menu shadow="md" width={200} opened={opened} onChange={setIsOpened}>
@@ -12,8 +13,13 @@ export const FilterDropdown = () => {
       </Menu.Target>
 
       <Menu.Dropdown>
-        <Menu.Label>Product Line</Menu.Label>
-        <Menu.Item icon={<IconSettings size={14} />}>Settings</Menu.Item>
+        <Menu.Label className={classes.productLabel}>Product Line</Menu.Label>
+        <Checkbox label="UniFi" className={classes.checkboxItems} size="xs" />
+        <Checkbox label="UniFi LTE" className={classes.checkboxItems} size="xs" />
+        <Checkbox label="UniFi Protect" className={classes.checkboxItems} size="xs" />
+        <Checkbox label="UniFi Access" className={classes.checkboxItems} size="xs" />
+        <Checkbox label="airMax" className={classes.checkboxItems} size="xs" />
+        <Checkbox label="EdgeMax" className={classes.checkboxItems} size="xs" />
       </Menu.Dropdown>
     </Menu>
   );
